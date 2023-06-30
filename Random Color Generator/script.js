63856fd13d304f38f55293822ab8975459c94182
Param Thakkar <128291516+ParamThakkar123@users.noreply.github.com>
2023-06-30 18:42:28 +0530

    Create script.js

---
+++ Random Color Generator/script.js
@@ -0,0 +1,15 @@
+const getColor = () => {
+    const randomNumber = Math.floor(Math.random() * 16777215)
+    const randomCode = "#" + randomNumber.toString(16)
+    document.body.style.backgroundColor = randomCode;
+    document.getElementById("color-code").innerText = randomCode
+
+    navigator.clipboard.write(randomCode)
+}
+
+document.getElementById("btn").addEventListener(
+    "click",
+    getColor
+)
+
+getColor()
