bec676ea1423e510f972b03f3a8310b4a1d38f63
paramthakkar123 <paramthakkar864@gmail.com>
2024-04-27 00:00:09 +0530

    backend added

--- Chatui backend/api.py
+++ Chatui backend/api.py
@@ -1,61 +1,59 @@
-from fastapi import FastAPI, HTTPException
-from pydantic import BaseModel
-import torch
+from fastapi import FastAPI
+from transformers import pipeline
+from langchain_community.llms.huggingface_pipeline import HuggingFacePipeline
 from langchain.memory import MongoDBChatMessageHistory, ConversationBufferMemory
 from langchain.chains import ConversationChain
 from langchain.prompts.prompt import PromptTemplate
 from transformers import AutoModelForCausalLM, AutoTokenizer
-import os
 import uvicorn
 
 uri = "mongodb+srv://paramthakkar864:paramthakkar864@cluster0.wmszguq.mongodb.net/GPTdb?retryWrites=true&w=majority&appName=Cluster0"
-
-device = "cuda"
-model = AutoModelForCausalLM.from_pretrained("smallstepai/Misal-1B-instruct-v0.1", torch_dtype=torch.bfloat16, device_map='auto')
-tokenizer = AutoTokenizer.from_pretrained("smallstepai/Misal-1B-instruct-v0.1")
-
-def ask_misal(model, tokenizer, instruction, inputs='', system_prompt='', max_new_tokens=200, device='cuda'):
-    ip = dict(system_prompt=system_prompt, instruction=instruction, inputs=inputs)
-    model_inputs = tokenizer.apply_chat_template(ip, return_tensors='pt')
-    outputs = model.generate(model_inputs.to(device), max_new_tokens=max_new_tokens)
-    response = tokenizer.decode(outputs[0]).split('### Response:')[1].strip()
-    return response
-
-# FastAPI app setup
 app = FastAPI()
+tokenizer = AutoTokenizer.from_pretrained("smallstepai/Misal-1B-instruct-v0.1")
+model = AutoModelForCausalLM.from_pretrained("smallstepai/Misal-1B-instruct-v0.1")
 
-# Define a schema for the POST request
-class ChatInput(BaseModel):
-    input: str
-    session: str
+text_pipeline = pipeline(
+    "text-generation",
+    model=model,
+    tokenizer=tokenizer,
+    max_new_tokens=1024,
+    temperature=0,
+)
 
-DEFAULT_TEMPLATE = """You are a chatbot that answers users' questions appropriately and accurately."""
+llm = HuggingFacePipeline(pipeline=text_pipeline, model_kwargs={"temperature": 0})
 
-@app.post("/chat")
+_DEFAULT_TEMPLATE = """
+आपण एक प्रामाणिक सहाय्यक आहात. वापरकर्त्यांच्या प्रश्नांची उत्तरे शक्य तितक्या योग्य आणि अचूकपणे द्या.
+History: \"{history}\"
+Query: \"{input}\"
+"""
+
+@app.post('/chat')
 def chatbot(input, session):
-    user_input = input
-    session_id = session
-    message_history = MongoDBChatMessageHistory(connection_string=uri, session_id=session_id)
-    if message_history.messages:
-        memories = ConversationBufferMemory(k=3)
-        for message in message_history.messages:
-            if message.type == 'user':
-                memories.save_context({"input": message.content}, {})
-            elif message.type == 'ai':
-                memories.save_context({}, {"output": message.content})
+    message_history = MongoDBChatMessageHistory(
+        connection_string=uri, session_id=session
+    )
 
-        # Create a new conversation chain with memory
-        conversation = ConversationChain(llm=model, prompt=PromptTemplate(input_variables=["history", "input"], template=DEFAULT_TEMPLATE), memory=memories)
-        conv = conversation.predict(input=user_input, link=session_id)
-        message_history.add_user_message(user_input)
+    PROMPT = PromptTemplate(input_variables=["history", "input"], template=_DEFAULT_TEMPLATE,)
+
+    memories = ConversationBufferMemory(k=3, return_messages=True)
+
+    if len(message_history.messages):
+        memories.save_context(
+            {"input": message_history.messages[0].content},
+            {"output": message_history.messages[1].content}
+        )
+        conversation = ConversationChain(llm=llm, verbose=False, prompt=PROMPT, memory=memories)
+        conv = conversation.predict(input=input)
+        message_history.add_user_message(input)
         message_history.add_ai_message(conv)
-        return {"response": conv}
+        return conv
     else:
-        memories = ConversationBufferMemory(k=3)
-        response = ask_misal(model, tokenizer, instruction=DEFAULT_TEMPLATE, inputs=user_input, max_new_tokens=200)
-        message_history.add_user_message(user_input)
-        message_history.add_ai_message(response)
-        return {"response": response}
+        conversation = ConversationChain(llm=llm, verbose=False, prompt=PROMPT, memory=memories)
+        conv = conversation.predict(input=input)
+        message_history.add_user_message(input)
+        message_history.add_ai_message(conv)
+        return conv
     
 if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=5000, log_level="info")
+    uvicorn.run(app, host='127.0.0.1', port=8000)
